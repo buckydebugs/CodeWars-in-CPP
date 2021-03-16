c4dc0b836c6e62cdbc9f85df0f94e3714849b7b4
buckydebugs <73716542+buckydebugs@users.noreply.github.com>
2021-03-17 00:42:03 +0530

    8 kyu Is he gonna survive?
    A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
    
    Return True if yes, False otherwise :)

---
+++ 8kyuIshegonnasurvive.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+bool hero(int bullets, int dragons) {
+  return (bullets >= 2*dragons) ? true : false;
+}
+int main()
+{
+    hero(4, 8);
+    return 0;
+}
